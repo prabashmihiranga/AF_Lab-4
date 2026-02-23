d40b855d5c17bd79681bf81b1d0c5911190f8f82
prabashmihiranga <prabashmihiranga@gmail.com>
2026-02-23 12:41:38 +0530

    Add web server feature

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-const fs = require('fs'); 
-fs.writeFile('file.txt', 'Hello World!', function (err) { 
-  if (err) throw err; 
-  console.log('File saved!'); 
-}); 
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
